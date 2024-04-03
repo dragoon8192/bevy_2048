8e0d0c4e0ac0a91123123e8a4a1efed1ea2eac99
Dragoon8192 <dragoon8192@gmail.com>
2024-04-04 02:32:14 +0900

    fix: 関数名の変更と引数の参照の修正。

--- src/systems/position.rs
+++ src/systems/position.rs
@@ -5,19 +5,28 @@ use std::collections::BTreeSet;
 use crate::components::position::Position;
 use crate::constants::SIDE_LENGTH;
 
-fn get_positions_set(lens: &mut QueryLens<&Position>) -> BTreeSet<Position> {
+fn get_positions_set(mut lens: QueryLens<&Position>) -> BTreeSet<Position> {
     let query: Query<'_, '_, &Position> = lens.query();
     let iter = query.iter().cloned();
     return BTreeSet::from_iter(iter);
 }
 
-fn positions_univ_set() -> BTreeSet<Position> {
+fn generate_universal_position_set() -> BTreeSet<Position> {
     return BTreeSet::from_iter(iproduct!(0..SIDE_LENGTH, 0..SIDE_LENGTH).map(Position::from));
 }
 
-pub fn get_positions_complement_set(lens: &mut QueryLens<&Position>) -> BTreeSet<Position> {
+pub fn check_positions_are_full(mut lens: QueryLens<&Position>) -> bool {
+    let positions_set = get_positions_set(lens);
+    let univ_set = generate_universal_position_set();
+    return positions_set == univ_set;
+}
+
+pub fn get_positions_complement_set(mut lens: QueryLens<&Position>) -> BTreeSet<Position> {
     let query: Query<'_, '_, &Position> = lens.query();
     let iter = query.iter().cloned();
     let set: BTreeSet<Position> = BTreeSet::from_iter(iter);
-    return positions_univ_set().difference(&set).cloned().collect();
+    return generate_universal_position_set()
+        .difference(&set)
+        .cloned()
+        .collect();
 }
