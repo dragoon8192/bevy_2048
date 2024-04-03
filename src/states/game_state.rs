4b29c941a6613a4433fcdff8cf7c89850d8b5b98
Dragoon8192 <dragoon8192@gmail.com>
2024-04-04 02:22:10 +0900

    update: 関数 check_game_over を追加。

--- src/states/game_state.rs
+++ src/states/game_state.rs
@@ -1,4 +1,10 @@
+use bevy::ecs::query;
+use bevy::ecs::system::QueryLens;
 use bevy::prelude::*;
+
+use crate::components::position::Position;
+use crate::components::tile::Tile;
+use crate::systems::position::check_positions_are_full;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 pub enum GameState {
@@ -8,6 +14,14 @@ pub enum GameState {
     GameOver,
 }
 
-pub fn return_to_move_state(mut next_state: ResMut<NextState<GameState>>) {
-    next_state.set(GameState::Move);
+pub fn check_game_over(
+    mut query: Query<&Position, With<Tile>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    let mut lens: QueryLens<&Position> = query.transmute_lens();
+    if check_positions_are_full(&mut lens) {
+        next_state.set(GameState::GameOver);
+    } else {
+        next_state.set(GameState::Move);
+    }
 }
