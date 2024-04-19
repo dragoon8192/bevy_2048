935346cb7d697a67683e431d873cf348f229eca7
Dragoon8192 <dragoon8192@gmail.com>
2024-04-20 00:19:45 +0900

    const SIDE_LENGTH -> GRID_WIDTH, GRID_HEIGHT.

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,9 @@
 use bevy::prelude::Vec2;
 
 pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4));
-pub const SIDE_LENGTH: usize = 4;
+pub const GRID_WIDTH: usize = 4;
+pub const GRID_HEIGHT: usize = 5;
 pub const TILE_SIZE: f32 = 60.0;
-pub const TILE_FONT_SIZE: f32 = 50.0;
 pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE, TILE_SIZE));
+pub const TILE_FONT_SIZE: f32 = 50.0;
 pub const WINDOW_SIZE: f32 = 500.0;
