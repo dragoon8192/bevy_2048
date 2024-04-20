bbf01f85dd86e1f46409c7761f9747dcc687ba89
Dragoon8192 <dragoon8192@gmail.com>
2024-04-20 21:42:47 +0900

    update: Set next_state.

--- src/events/player_input_event.rs
+++ src/events/player_input_event.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::states::game_state::GameState;
+
 #[derive(Event, PartialEq, Eq)]
 pub enum PlayerInputEvent {
     Left,
@@ -11,14 +13,20 @@ pub enum PlayerInputEvent {
 pub fn input_from_keyboard(
     keyboard: Res<ButtonInput<KeyCode>>,
     mut input_evw: EventWriter<PlayerInputEvent>,
+    mut next_state: ResMut<NextState<GameState>>,
 ) {
+    dbg!("System: input_from_keyboard");
     if keyboard.just_pressed(KeyCode::ArrowLeft) {
         input_evw.send(PlayerInputEvent::Left);
+        next_state.set(GameState::Calculate);
     } else if keyboard.just_pressed(KeyCode::ArrowRight) {
         input_evw.send(PlayerInputEvent::Right);
+        next_state.set(GameState::Calculate);
     } else if keyboard.just_pressed(KeyCode::ArrowUp) {
         input_evw.send(PlayerInputEvent::Up);
+        next_state.set(GameState::Calculate);
     } else if keyboard.just_pressed(KeyCode::ArrowDown) {
         input_evw.send(PlayerInputEvent::Down);
+        next_state.set(GameState::Calculate);
     }
 }
