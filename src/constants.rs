14656efbe0a2b6a0ca1481337064170e5f386b8f
Dragoon8192 <dragoon8192@gmail.com>
2024-04-23 17:33:48 +0900

    fix: GRID_HEIGHT = 4.

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@ use bevy::prelude::Vec2;
 
 pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4));
 pub const GRID_WIDTH: usize = 4;
-pub const GRID_HEIGHT: usize = 5;
+pub const GRID_HEIGHT: usize = 4;
 pub const TILE_SIZE: f32 = 60.0;
 pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE, TILE_SIZE));
 pub const TILE_FONT_SIZE: f32 = 50.0;
