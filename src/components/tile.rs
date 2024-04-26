aae8d64a294f2e7b2bb17eb08b86085cb9c63cd5
Dragoon8192 <dragoon8192@gmail.com>
2024-04-26 20:31:16 +0900

    update: Color を数字によってグラデーションさせた.

--- src/components/tile.rs
+++ src/components/tile.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::constants::{TILE_COLOR_0, TILE_COLOR_1};
+
 #[derive(Component, Clone, Copy, PartialEq, Eq)]
 pub struct Tile(pub u64);
 
@@ -11,13 +13,9 @@ impl Tile {
 
 impl From<Tile> for Color {
     fn from(Tile(num): Tile) -> Self {
-        match num {
-            2 => Color::GOLD,
-            4 => Color::ORANGE,
-            8 => Color::ORANGE_RED,
-            _ => Color::RED,
-            // TODO
-        }
+        // num = 2 -> r = 0.0, num >= 2048 -> r = 1.0
+        let r: f32 = (u64::BITS - num.leading_zeros() - 2).min(10) as f32 / 10.0;
+        return TILE_COLOR_0 * (1.0 - r) + TILE_COLOR_1 * r;
     }
 }
 
--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::Vec2;
+use bevy::{prelude::Vec2, render::color::Color};
 
 pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4));
 pub const GRID_WIDTH: usize = 4;
@@ -7,3 +7,6 @@ pub const TILE_SIZE: f32 = 60.0;
 pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE, TILE_SIZE));
 pub const TILE_FONT_SIZE: f32 = 50.0;
 pub const WINDOW_SIZE: f32 = 500.0;
+
+pub const TILE_COLOR_0: Color = Color::rgb(0.0, 0.922, 0.5);
+pub const TILE_COLOR_1: Color = Color::rgb(0.0, 0.5, 0.922);
