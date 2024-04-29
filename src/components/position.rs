f24de42953aae2a59a18d041735de667a97f5630
Dragoon8192 <dragoon8192@gmail.com>
2024-04-29 18:01:42 +0900

    update: BOARD 関連の定数.

--- src/components/position.rs
+++ src/components/position.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
-use crate::constants::GRID_HEIGHT;
-use crate::constants::GRID_WIDTH;
-use crate::constants::TILE_SIZE;
+use crate::constants::{
+    GRID_HEIGHT, GRID_WIDTH, TILE_HEIGHT, TILE_MARGIN_HORIZONTAL, TILE_MARGIN_VERTICAL, TILE_WIDTH,
+};
 
 #[derive(Component, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Debug)]
 pub struct Position {
@@ -15,8 +15,10 @@ impl Position {
         return Self { x, y };
     }
     pub fn to_transform(&self, z: f32) -> Transform {
-        let x = (-(GRID_WIDTH as f32) / 2.0 + self.x as f32 + 0.5) * (TILE_SIZE * 1.05);
-        let y = (-(GRID_HEIGHT as f32) / 2.0 + self.y as f32 + 0.5) * (TILE_SIZE * 1.05);
+        let x = (-(GRID_WIDTH as f32) / 2.0 + self.x as f32 + 0.5)
+            * (TILE_WIDTH + TILE_MARGIN_HORIZONTAL);
+        let y = (-(GRID_HEIGHT as f32) / 2.0 + self.y as f32 + 0.5)
+            * (TILE_HEIGHT + TILE_MARGIN_VERTICAL);
         return Transform::from_xyz(x, y, z);
     }
     pub fn shift(&mut self, (x, y): (isize, isize)) {
--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,26 @@
-use bevy::{prelude::Vec2, render::color::Color};
+use bevy::{math::f32, prelude::Vec2, render::color::Color};
 
-pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4));
+pub const WINDOW_SIZE: f32 = 500.0;
+
 pub const GRID_WIDTH: usize = 4;
 pub const GRID_HEIGHT: usize = 4;
-pub const TILE_SIZE: f32 = 60.0;
-pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_SIZE, TILE_SIZE));
+
+pub const TILE_WIDTH: f32 = 60.0;
+pub const TILE_HEIGHT: f32 = 60.0;
+pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_WIDTH, TILE_HEIGHT));
+pub const TILE_MARGIN_HORIZONTAL: f32 = 5.0;
+pub const TILE_MARGIN_VERTICAL: f32 = 5.0;
 pub const TILE_FONT_SIZE: f32 = 60.0;
-pub const WINDOW_SIZE: f32 = 500.0;
+
+pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(
+    TILE_WIDTH * GRID_WIDTH as f32
+        + TILE_MARGIN_HORIZONTAL * (GRID_WIDTH - 1) as f32
+        + BOARD_PADDING * 2.0,
+    TILE_HEIGHT * GRID_HEIGHT as f32
+        + TILE_MARGIN_VERTICAL * (GRID_HEIGHT - 1) as f32
+        + BOARD_PADDING * 2.0,
+));
+pub const BOARD_PADDING: f32 = 22.5;
 
 pub const TILE_COLOR_0: Color = Color::rgb(0.0, 0.922, 0.5);
 pub const TILE_COLOR_1: Color = Color::rgb(0.0, 0.5, 0.922);
