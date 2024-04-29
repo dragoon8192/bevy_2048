aaf75a777f48c1b87caa7750a353d5fbfb2b8961
Dragoon8192 <dragoon8192@gmail.com>
2024-04-29 18:05:34 +0900

    fix.

--- src/constants.rs
+++ src/constants.rs
@@ -8,8 +8,8 @@ pub const GRID_HEIGHT: usize = 4;
 pub const TILE_WIDTH: f32 = 60.0;
 pub const TILE_HEIGHT: f32 = 60.0;
 pub const TILE_SIZE_2D: Option<Vec2> = Some(Vec2::new(TILE_WIDTH, TILE_HEIGHT));
-pub const TILE_MARGIN_HORIZONTAL: f32 = 5.0;
-pub const TILE_MARGIN_VERTICAL: f32 = 5.0;
+pub const TILE_MARGIN_HORIZONTAL: f32 = 8.0;
+pub const TILE_MARGIN_VERTICAL: f32 = 8.0;
 pub const TILE_FONT_SIZE: f32 = 60.0;
 
 pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(
@@ -20,7 +20,7 @@ pub const BOARD_SIZE_2D: Option<Vec2> = Some(Vec2::new(
         + TILE_MARGIN_VERTICAL * (GRID_HEIGHT - 1) as f32
         + BOARD_PADDING * 2.0,
 ));
-pub const BOARD_PADDING: f32 = 22.5;
+pub const BOARD_PADDING: f32 = 18.0;
 
 pub const TILE_COLOR_0: Color = Color::rgb(0.0, 0.922, 0.5);
 pub const TILE_COLOR_1: Color = Color::rgb(0.0, 0.5, 0.922);
