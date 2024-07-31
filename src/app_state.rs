82fcef8a9d1f84d590e61ed5a3d79bf8a3f6d497
Dragoon8192 <dragoon8192@gmail.com>
2024-07-31 13:47:46 +0900

    WIP: game 本体を src/app_state/game/ へ移動した.

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,2 +1,35 @@
+pub mod game;
 pub mod score_board;
 pub mod title_menu;
+
+use bevy::ecs::{
+    schedule::{NextState, States},
+    system::ResMut,
+};
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum App {
+    #[default]
+    TitleMenu,
+    ScoreBoard,
+    Game(Game),
+}
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+pub enum Game {
+    #[default]
+    Spawn,
+    Input,
+    Calculate,
+    Movement,
+    GameOver,
+}
+
+impl App {
+    pub fn set_next(self) -> impl FnMut(ResMut<NextState<App>>) {
+        return move |mut next: ResMut<NextState<App>>| {
+            dbg!(self);
+            next.set(self);
+        };
+    }
+}
